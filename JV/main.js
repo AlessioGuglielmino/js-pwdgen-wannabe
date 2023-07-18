ca868f8e32853fd721eb6c52ea49fe4b2173e326
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-18 16:27:04 +0200

    java

---
+++ JV/main.js
@@ -0,0 +1,7 @@
+const username = prompt("inserisci il tuo nome");
+const surname = prompt("inserisci il tuo cognome");
+const color = prompt("inserisci il tuo colore preferito");
+
+const dataUser = username + surname + color;
+
+document.getElementById("message-to-user").innerHTML = dataUser;
