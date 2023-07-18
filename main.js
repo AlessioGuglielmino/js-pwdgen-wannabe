4b14bdb9e956125678d7f5a4f464bc9b82236cc9
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-18 16:38:12 +0200

    javascript-exercise-done

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const username = prompt("inserisci il tuo nome");
 const surname = prompt("inserisci il tuo cognome");
 const color = prompt("inserisci il tuo colore preferito");
 
-const dataUser = username + surname + color;
+const dataUser = username + surname + color + "21";
 
 document.getElementById("message-to-user").innerHTML = dataUser;
---
+++ redme.md
@@ -0,0 +1,4 @@
+Chiedo all'utente il suo **nome**
+Chiedo il suo cognome,
+Chiedo il suo colore preferito
+Infine scrivo sulla pagina nomecognomecolorepreferito21
