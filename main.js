4ce77a8230e22d5f5cdc88c0afcdcf097a04559b
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-07-18 16:50:16 +0200

    js-try

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ const surname = prompt("inserisci il tuo cognome");
 const color = prompt("inserisci il tuo colore preferito");
 
 const dataUser = username + surname + color + "21";
+const welcomeUser = ` <h3>Benvenuto! ${dataUser} </h3>;
+`;
 
-document.getElementById("message-to-user").innerHTML = dataUser;
+document.getElementById("message-to-user").innerHTML = welcomeUser;
